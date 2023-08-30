c6cb6022f6325253ced3708910bbca0c3ff191f9
Vicente Andres Vasquez Gonzalez <vicevasquez95@gmail.com>
2023-08-29 23:24:12 -0400

    fix(shared/components): add logic to input text (#2)
    Co-Authored-By: Mauricio Herrera Ortega <ronalli56@gmail.com>

--- src/app/shared/components/input-text/input-text.component.ts
+++ src/app/shared/components/input-text/input-text.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {Component, Input, OnInit} from '@angular/core';
+import { FormControl, Validators} from '@angular/forms';
+
+type InputTextType = 'email' | 'password' | 'text'
 
 @Component({
   selector: 'app-input-text',
@@ -10,11 +12,31 @@ export class InputTextComponent  implements OnInit {
 
   constructor() { }
 
-  favoriteColorControl = new FormControl()
+  readonly textControl = new FormControl('', control => {
+    control.addValidators(Validators.required)
+    switch (this.type) {
+      case "email":
+        control.addValidators(Validators.email)
+        break;
+      case "password":
+        control.addValidators(Validators.min(8))
+        break;
+      case "text":
+        break;
+    }
+    return null
+  })
 
-  ngOnInit() {}
+  @Input() type: InputTextType = 'text'
+
+  ngOnInit() {
+  }
 
   print(){
-    console.log(this.favoriteColorControl)
+    console.log(this.textControl)
+    console.log(this.textControl.errors)
+    console.log(this.textControl.status)
+    console.log(this.textControl.statusChanges)
+    console.log(this.textControl.value)
   }
 }
