db9a69f8b0186f0be5481b2499af4c823a8de86c
GodDoesNotPlayDice <isaacgamerhd95@gmail.com>
2023-08-31 01:02:00 -0400

    fix(home): tabs foundation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
@@ -15,6 +11,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule),
+  // },
 ];
 
 @NgModule({
