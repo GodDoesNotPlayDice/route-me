bfe526ae65194fc2578c4546e9257b3790c35e38
GodDoesNotPlayDice <isaacgamerhd95@gmail.com>
2023-08-31 22:05:41 -0400

    fix(outline button): add style and logic, modify fonts to filled button and input text

--- src/app/shared/components/outlined-button/outlined-button.component.ts
+++ src/app/shared/components/outlined-button/outlined-button.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-outlined-button',
   templateUrl: './outlined-button.component.html',
   styleUrls: ['./outlined-button.component.scss'],
 })
-export class OutlinedButtonComponent  implements OnInit {
-
+export class OutlinedButtonComponent {
+  @Input() contentText: string = '';
   constructor() { }
-
-  ngOnInit() {}
-
 }
