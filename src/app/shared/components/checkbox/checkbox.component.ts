5f21224eab594fae7f884d04a1b24afb3db5cedf
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-08-31 20:11:19 -0400

    fix(shared/checkbox): revert checkbox

--- src/app/shared/components/checkbox/checkbox.component.ts
+++ src/app/shared/components/checkbox/checkbox.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core'
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
 })
-export class CheckboxComponent  implements OnInit {
+export class CheckboxComponent {
+
+  @Input() isChecked: boolean = false;
+  @Input() contentText : string = "";
+  @Output() isCheckedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor() { }
-
-  ngOnInit() {}
-
+  onCheckboxChange(): void {
+    this.isChecked = !this.isChecked;
+    this.isCheckedChange.emit(this.isChecked);
+  }
 }
