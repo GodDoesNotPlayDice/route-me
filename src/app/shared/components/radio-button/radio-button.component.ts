ed6363f8046f38315c26a212fe50c78901106e53
GodDoesNotPlayDice <isaacgamerhd95@gmail.com>
2023-09-01 22:22:20 -0400

    fix(radio-buttons): add radio buttons

--- src/app/shared/components/radio-button/radio-button.component.ts
+++ src/app/shared/components/radio-button/radio-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-radio-button',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./radio-button.component.scss'],
 })
 export class RadioButtonComponent  implements OnInit {
+  @Input() contentText: string = "";
 
   constructor() { }
 
