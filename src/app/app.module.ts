cc9089d571b9648916f02552ff60e64349fa48cf
GodDoesNotPlayDice <isaacgamerhd95@gmail.com>
2023-09-03 02:35:46 -0300

    fix(user-service): add auth-service and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,55 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AuthRepository} from "../package/user/domain/repository/AuthRepository";
+import {AuthDataMemory} from "../package/user/infrastructure/AuthDataMemory";
+import {LoginUser} from "../package/user/application/LoginUser";
+import {RegisterUser} from "../package/user/application/RegisterUser";
+import {AuthService} from "./services/auth/auth.service";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,  BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+    {
+      provide: AuthRepository,
+      useFactory: () => {
+        return new AuthDataMemory()
+      },
+      deps: []
+    },
+    {
+      provide: LoginUser,
+      useFactory: (authRepository : AuthRepository) =>{
+        return new LoginUser(authRepository)
+      },
+      deps: [AuthRepository]
+    },
+    {
+      provide: RegisterUser,
+      useFactory: (authRepository : AuthRepository) =>{
+        return new RegisterUser(authRepository)
+      },
+      deps: [AuthRepository]
+    },
+    {
+      provide: AuthService,
+      useFactory: (
+        registerUser :RegisterUser,
+        loginUser : LoginUser
+      ) => {
+        return new AuthService(
+          loginUser,
+          registerUser
+        )
+      },
+      deps: [RegisterUser, LoginUser]
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import {Err, None, Ok, Option, Result, Some} from "oxide.ts";
+import {LoginUser} from "../../../package/user/application/LoginUser";
+import {RegisterUser} from "../../../package/user/application/RegisterUser";
+import {User} from "../../../package/user/domain/entities/User";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private loginUser : LoginUser,
+    private registerUser : RegisterUser,
+    ) { }
+
+  currentUser : Option<User> = None
+
+  async login(email : string, password: string): Promise<Result<boolean, string>> {
+
+    const result = await this.loginUser.execute(email, password)
+
+    if (result.isErr()){
+
+      return Promise.resolve(Err(result.unwrapErr()));
+    }
+
+    this.currentUser = Some(result.unwrap())
+    return Promise.resolve(Ok(true));
+  }
+
+   async register(
+     id :string,
+     email :string,
+     name :string,
+     lastName :string,
+     password :string,
+     description :string,
+     phone :string,
+     birthDay :string
+   ): Promise<Result<boolean, string>> {
+     const result = await this.registerUser.execute(
+       id ,
+       email ,
+       name ,
+       lastName ,
+       password ,
+       description,
+       phone ,
+       birthDay
+     )
+     if (result.isErr()){
+       return Promise.resolve(Err(result.unwrapErr()));
+     }
+     return Promise.resolve(Ok(true));
+  }
+}
