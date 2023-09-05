ae734d26f8addbb69d77e35c20c31cbbd15eb028
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-09-04 23:27:44 -0300

    fix(login): modify checkbox and forms

--- src/app/reset-password/reset-password.page.ts
+++ src/app/reset-password/reset-password.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ViewChild
+} from '@angular/core'
+import { FormGroup } from '@angular/forms'
+import { ViewDidEnter } from '@ionic/angular'
+import { InputTextComponent } from 'src/app/shared/components/input-text/input-text.component'
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
 })
-export class ResetPasswordPage implements OnInit {
+export class ResetPasswordPage implements ViewDidEnter{
 
-  constructor() { }
+  @ViewChild('pw1') password1Input!: InputTextComponent
+  @ViewChild('pw2') password2Input!: InputTextComponent
+  // formGroup! : FormGroup
 
-  ngOnInit() {
+  public ionViewDidEnter(): void {
+    // this.formGroup = new FormGroup([
+    //   this.password1Input.textControl,
+    //   this.password2Input.textControl
+    // ],{
+    //   validators: (control) => {
+    //     const password1 = control.get('pw1')?.value
+    //     const password2 = control.get('pw2')?.value
+    //     if(password1 === password2) return null
+    //     return { passwordMatch: true }
+    //   }
+    // })
   }
 
 }
--- src/app/shared/components/checkbox/checkbox.component.ts
+++ src/app/shared/components/checkbox/checkbox.component.ts
@@ -1,36 +1,46 @@
 import {
   Component,
+  ContentChild,
+  ElementRef,
   EventEmitter,
   Input,
-  Output
+  Output,
+  ViewChild
 } from '@angular/core'
-import {FormControl, Validators} from "@angular/forms";
+import {
+  FormControl,
+  Validators
+} from '@angular/forms'
 
-@Component({
-  selector: 'app-checkbox',
+@Component( {
+  selector   : 'app-checkbox',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.scss'],
-})
+  styleUrls  : [ './checkbox.component.scss' ]
+} )
 export class CheckboxComponent {
 
-  @Input() isChecked: boolean = false;
-  @Input() errorText: string = ''
-  @Input() contentText : string = "";
-  @Output() isCheckedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() activeError: boolean = false
 
-  constructor() { }
+  @Input() isChecked: boolean = false
+
+  @Input() errorText: string = ''
+
+  @Input() contentText: string = ''
+
+  @Output() isCheckedChange: EventEmitter<boolean> = new EventEmitter<boolean>()
 
   readonly checkboxControl = new FormControl( false, control => {
-      if ( this.errorText.length > 0 && !control.value ) {
-        control.addValidators( Validators.requiredTrue )
-        return { required: true }
-      }
-      return null
-    } )
+    if ( this.activeError && !control.value ) {
+      control.addValidators( Validators.requiredTrue )
+      return { required: true }
+    }
+    return null
+  } )
 
   onCheckboxChange(): void {
-    this.isChecked = !this.isChecked;
-    this.isCheckedChange.emit(this.isChecked);
-    //this.checkboxControl.patchValue( this.isChecked )
+    this.isChecked = !this.isChecked
+    this.isCheckedChange.emit( this.isChecked )
+    this.checkboxControl.patchValue( this.isChecked )
+    this.checkboxControl.updateValueAndValidity()
   }
 }
