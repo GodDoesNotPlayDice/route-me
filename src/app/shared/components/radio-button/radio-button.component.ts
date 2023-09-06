2e5a75ca60bd1de6cf5e903b0a7343975bf31e4d
Vicente Andres Vasquez Gonzalez <vicevasquez95@gmail.com>
2023-09-06 17:21:17 -0300

    fix(radio-button): add radio button logic (#34)

--- src/app/shared/components/radio-button/radio-button.component.ts
+++ src/app/shared/components/radio-button/radio-button.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-radio-button',
   templateUrl: './radio-button.component.html',
   styleUrls: ['./radio-button.component.scss'],
 })
-export class RadioButtonComponent  implements OnInit {
-  @Input() contentText: string = "";
+export class RadioButtonComponent{
+  @Output() isCheckedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor() { }
+  @Input() radioButtons: RadioButtonData[] = [];
+  @Input() name! : string
 
-  ngOnInit() {}
+  isChecked: boolean = false;
 
+  readonly radioControl = new FormControl(false, [Validators.requiredTrue]);
+
+  onRadioChange($event: Event) {
+    if (!this.isChecked) {
+      this.isChecked = true;
+    }
+    this.isCheckedChange.emit(this.isChecked);
+    this.radioControl.patchValue(this.isChecked);
+    this.radioControl.updateValueAndValidity();
+    console.log(this.radioControl.valid)
+    console.log(this.radioControl.errors)
+  }
+}
+
+export interface RadioButtonData {
+  icon : string
+  name : string
 }
