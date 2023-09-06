91fba6ddbb8f19cf6148b0edd59cd4bf2d963bda
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-09-06 00:49:53 -0300

    fix(date-selector-component): add date selector component

--- src/app/shared/components/date-selector/date-selector.component.ts
+++ src/app/shared/components/date-selector/date-selector.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-date-selector',
   templateUrl: './date-selector.component.html',
   styleUrls: ['./date-selector.component.scss'],
 })
-export class DateSelectorComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class DateSelectorComponent {
 }
