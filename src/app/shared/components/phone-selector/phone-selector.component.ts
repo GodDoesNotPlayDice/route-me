2b4abafb5a13e2276ebb49f8b393d5a3ddf21587
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-09-09 10:45:05 -0300

    fix(phone-selector): add phone selector component (#62)

---
+++ src/app/shared/components/phone-selector/phone-selector.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-selector',
+  templateUrl: './phone-selector.component.html',
+  styleUrls: ['./phone-selector.component.scss'],
+})
+export class PhoneSelectorComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
