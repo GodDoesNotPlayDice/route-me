f832e39ecba05dd95abbc1872a90cf46487ecfcc
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-09-09 10:48:08 -0300

    fix(step1-page): add form logic

--- src/app/register/step1/step1.page.ts
+++ src/app/register/step1/step1.page.ts
@@ -1,7 +1,12 @@
 import {
   Component,
+  ViewChild
 } from '@angular/core'
+import { FormGroup } from '@angular/forms'
+import { ViewDidEnter } from '@ionic/angular'
 import { Store } from '@ngrx/store'
+import { CheckboxComponent } from 'src/app/shared/components/checkbox/checkbox.component'
+import { InputTextComponent } from 'src/app/shared/components/input-text/input-text.component'
 import { AppState } from 'src/app/state/app.state'
 import { notifyStep } from 'src/app/state/stepper/step.actions'
 
@@ -10,13 +15,38 @@ import { notifyStep } from 'src/app/state/stepper/step.actions'
   templateUrl: './step1.page.html',
   styleUrls: ['./step1.page.scss'],
 })
-export class Step1Page {
+export class Step1Page implements ViewDidEnter{
 
   constructor(private store : Store<AppState>) {
   }
 
+  @ViewChild('user') userInput!: InputTextComponent
+  @ViewChild('password') passwordInput!: InputTextComponent
+  @ViewChild('confirmpassword') passwordConfirmInput!: InputTextComponent
+  @ViewChild( 'check') checkbox!: CheckboxComponent
+
+  formGroup! : FormGroup
+
   public submit( $event: SubmitEvent ): void {
     $event.preventDefault()
+
+    this.formGroup.updateValueAndValidity()
+    this.formGroup.markAllAsTouched()
+
+    if(
+      !this.formGroup.valid
+    ) return
+
+    console.log("step 1 check")
     this.store.dispatch(notifyStep())
+  }
+
+  ionViewDidEnter() {
+    this.formGroup = new FormGroup([
+      this.userInput.textControl,
+      this.passwordInput.textControl,
+      this.passwordConfirmInput.textControl,
+      this.checkbox.checkboxControl
+    ])
   }
 }
