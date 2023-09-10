302106c5132c2bf50680c10ef3432d08c215458e
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-09-10 10:08:28 -0300

    fix(date-selector): modify validate logic (#57)

--- src/app/shared/components/date-selector/date-selector.component.ts
+++ src/app/shared/components/date-selector/date-selector.component.ts
@@ -6,28 +6,40 @@ import {
   FormControl,
   Validators
 } from '@angular/forms'
+import { ErrorStateMatcher } from '@angular/material/core'
 import { MatDatepickerInputEvent } from '@angular/material/datepicker'
 
-@Component({
-  selector: 'app-date-selector',
+@Component( {
+  selector   : 'app-date-selector',
   templateUrl: './date-selector.component.html',
-  styleUrls: ['./date-selector.component.scss'],
-})
+  styleUrls  : [ './date-selector.component.scss' ]
+} )
 export class DateSelectorComponent {
 
-  @Input() isValidDate: boolean = false
-  _date: Date                   = new Date()
-  readonly dateControl          = new FormControl( false, control => {
-    if ( !this.isValidDate ) {
+  dateSelected: Date | null = null
+
+  date18YearsAgo: Date = new Date( new Date().setFullYear( new Date().getFullYear() - 18 ) )
+
+  dateNow: Date        = new Date()
+  readonly dateControl = new FormControl( false, control => {
+    if ( this.dateSelected === null ) {
       control.addValidators( Validators.requiredTrue )
       return { required: true }
     }
+
+    if ( this.dateSelected > this.date18YearsAgo ) {
+      return { invalid: true }
+      }
+
     return null
   } )
 
-  onDate(event : MatDatepickerInputEvent<Date>) {
-    this.isValidDate = event.value !== null;
-    this.dateControl.patchValue( this.isValidDate )
+  onDate( event: MatDatepickerInputEvent<Date> ) {
+    this.dateSelected = event.value
+    this.dateControl.patchValue( this.dateSelected !== null )
+    this.dateControl.markAllAsTouched()
     this.dateControl.updateValueAndValidity()
   }
+
+  protected readonly ErrorStateMatcher = ErrorStateMatcher
 }
