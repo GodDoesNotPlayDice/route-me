c9eefc7f03700627b5f5e2bdc134de5202400a66
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-09-13 01:26:16 -0300

    fix(models-refactor): add separate file for each interface

---
+++ src/app/shared/models/CategoryTripHistory.ts
@@ -0,0 +1,6 @@
+import { DriverCardInfo } from 'src/app/shared/models/DriverCardInfo'
+
+export interface CategoryTripHistory {
+			name: string;
+			info: DriverCardInfo[];
+}
---
+++ src/app/shared/models/FilterButtonData.ts
@@ -0,0 +1,4 @@
+export interface FilterButtonData {
+	name: string,
+	imagen : string
+}
