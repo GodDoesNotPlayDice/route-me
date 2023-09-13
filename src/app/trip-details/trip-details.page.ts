8864c9f9cac6436f885319407240bc40ac95b15d
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-09-13 01:30:32 -0300

    fix(trip-details): add url service for back button functionality

--- src/app/trip-details/trip-details.page.ts
+++ src/app/trip-details/trip-details.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit
+} from '@angular/core'
+import { Router } from '@angular/router'
+import { UrlService } from 'src/app/services/url/url.service'
 
-@Component({
-  selector: 'app-trip-details',
-  templateUrl: './trip-details.page.html',
-  styleUrls: ['./trip-details.page.scss'],
-})
-export class TripDetailsPage implements OnInit {
+@Component( {
+	selector   : 'app-trip-details',
+	templateUrl: './trip-details.page.html',
+	styleUrls  : [ './trip-details.page.scss' ]
+} )
+export class TripDetailsPage implements OnInit{
 
-  constructor() { }
+	constructor( private urlService: UrlService,
+	private router : Router) {}
 
-  ngOnInit() {
-  }
+	prevHref: string = '/tabs/home'
 
+	public ngOnInit(): void {
+		this.urlService.previousUrl$.subscribe( ( url ) => {
+			this.prevHref = url
+		})
+
+		console.log(this.router.getCurrentNavigation()?.extras.state)
+	}
 }
