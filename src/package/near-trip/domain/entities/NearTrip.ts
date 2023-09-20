b3ea4421e7e746782dd9727fabf7543ebe053a90
Vicente Vasquez (iykyk) <vicevasquez95@gmail.com>
2023-09-20 12:26:52 -0300

    fix(near-trip-entity): add near trip entities

---
+++ src/package/near-trip/domain/entities/NearTrip.ts
@@ -0,0 +1,13 @@
+export class NearTrip {
+  constructor(
+    readonly id: string,
+    readonly tripID : string,
+    readonly startDate: Date,
+    readonly endDate: Date,
+    readonly startLocation: string,
+    readonly endLocation: string,
+    readonly categoryName: string,
+    readonly latitude: number,
+    readonly longitude: number
+  ) {}
+}
