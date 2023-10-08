c0c745318937744fa096dfa2ccb4b93daca30649
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-07 22:24:36 -0300

    fix(map-location-input): adapt map location input to activable circle

--- src/app/shared/components/map-location-input/map-location-input.component.ts
+++ src/app/shared/components/map-location-input/map-location-input.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common'
 import {
   Component,
-  HostListener,
   Input,
   ViewChild
 } from '@angular/core'
@@ -10,9 +9,10 @@ import {
   ReactiveFormsModule
 } from '@angular/forms'
 import { IonicModule } from '@ionic/angular'
+import { ActivableCircleComponent } from 'src/app/shared/components/activable-circle/activable-circle.component'
 import { FocusBlurDirective } from 'src/app/shared/directives/focus-blur.directive'
+import { SelectInputDirective } from 'src/app/shared/directives/select-input.directive'
 import { MapService } from 'src/app/shared/services/map.service'
-import { ulid } from 'ulidx'
 
 @Component( {
   standalone : true,
@@ -22,46 +22,31 @@ import { ulid } from 'ulidx'
     IonicModule,
     CommonModule,
     ReactiveFormsModule,
-    FocusBlurDirective
+    FocusBlurDirective,
+    ActivableCircleComponent,
+    SelectInputDirective
   ],
   styleUrls  : [ './map-location-input.component.scss' ]
 } )
 export class MapLocationInputComponent {
 
   @Input({required:true}) placeholder: string
-  id = ulid()
   isFocused: boolean = false
   locationText: string = ''
   @ViewChild( 'inputLocation' ) input!: HTMLInputElement
-
 
   constructor( private map: MapService ) {
     this.map.markerClick$.pipe()
         .subscribe(
           async ( location ) => {
-            if ( this.locationText.length > 0 ) return
             if ( location !== null && this.isFocused ) {
               this.locationText = `${ location[ 0 ].toFixed( 4 ) }, ${ location[ 1 ].toFixed( 4 ) }`
               this.mapLocationControl.patchValue( location )
               this.mapLocationControl.markAllAsTouched()
               this.mapLocationControl.updateValueAndValidity()
-              this.isFocused = true
+              this.isFocused = false
             }
           } )
-  }
-  @HostListener('document:click', ['$event'])
-  handleClick(event: MouseEvent) {
-    if ( event.target instanceof HTMLCanvasElement ){
-      return
-    }
-    if (  event.target instanceof HTMLInputElement ) {
-      if ( event.target.id === this.id ) {
-        this.isFocused = true
-      }
-      else{
-        this.isFocused = false
-      }
-    }
   }
   readonly mapLocationControl      = new FormControl<[ lng: number , lat: number ] | null>( null, control => {
     if ( this.locationText.length  === 0 ) {
@@ -69,4 +54,8 @@ export class MapLocationInputComponent {
     }
     return null
   } )
+
+  onActiveChange( $event: boolean ): void {
+    this.isFocused = $event
+  }
 }
