6eeee226dd93230dc9c2495cd8d384d5f957e528
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-13 01:57:01 -0300

    fix(rating-domain): add exceptions and mapper

--- src/package/rating/domain/models/rating-id.ts
+++ src/package/rating/domain/models/rating-id.ts
@@ -1,3 +1,9 @@
+import {
+  Err,
+  Ok,
+  Result
+} from 'oxide.ts'
+import { RatingIdInvalidException } from 'src/package/rating/domain/exceptions/rating-id-invalid-exception'
 import { z } from 'zod'
 
 export const RatingIDSchema = z.object( {
@@ -11,8 +17,19 @@ export interface RatingIDProps {
   value : string
 }
 
-export const newRatingID = (props : RatingIDProps): RatingID => {
-  return RatingIDSchema.parse( {
-    value : props.value
+/**
+ * Create a rating id instance
+ * @throws {RatingIdInvalidException} - if id is invalid
+ */
+export const newRatingID = (props : RatingIDProps): Result<RatingID, Error> => {
+  const result = RatingIDSchema.safeParse( {
+    value: props.value
   } )
+
+  if ( !result.success ) {
+    return Err( new RatingIdInvalidException() )
+  }
+  else {
+    return Ok( result.data )
+  }
 }
