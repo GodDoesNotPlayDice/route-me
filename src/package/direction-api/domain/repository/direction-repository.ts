0c2c52526d7d23135fe4d3ed06b3839719400d0a
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-12 22:45:58 -0300

    fix(direction-domain): add exceptions with refactor

--- src/package/direction-api/domain/repository/direction-repository.ts
+++ src/package/direction-api/domain/repository/direction-repository.ts
@@ -3,5 +3,5 @@ import { Direction } from 'src/package/direction-api/domain/models/direction'
 import { Position } from "src/package/position-api/domain/models/position";
 
 export abstract class DirectionRepository {
-    abstract getDirection( inicio: Position, final: Position ): Promise<Result<Direction, string>>
+    abstract getDirection( inicio: Position, final: Position ): Promise<Result<Direction, Error>>
 }
--- src/package/direction-api/infrastructure/mapbox/direction-map-box.ts
+++ src/package/direction-api/infrastructure/mapbox/direction-map-box.ts
@@ -6,6 +6,7 @@ import {
   Result
 } from 'oxide.ts'
 import { directionFromJson } from 'src/package/direction-api/application/direction-mapper'
+import { DirectionNotFoundException } from 'src/package/direction-api/domain/exceptions/direction-not-found-exception'
 import { Direction } from 'src/package/direction-api/domain/models/direction'
 import { DirectionRepository } from 'src/package/direction-api/domain/repository/direction-repository'
 import { Position } from 'src/package/position-api/domain/models/position'
@@ -14,15 +15,16 @@ export class DirectionMapBox implements DirectionRepository {
   constructor( private http: HttpClient ) {}
 
   async getDirection( inicio: Position,
-    final: Position ): Promise<Result<Direction, string>> {
+    final: Position ): Promise<Result<Direction, Error>> {
     const start    = `${ inicio.lng },${ inicio.lat }`
     const end      = `${ final.lng },${ final.lat }`
-    const response = await this.http.get(
-      `https://api.mapbox.com/directions/v5/mapbox/driving/${ start };${ end }?alternatives=true&geometries=geojson&overview=simplified&steps=false&access_token=${ environment.mapBoxApiKey }` )
-                               .toPromise()
+
+    const response = await this.http.get( `https://api.mapbox.com/directions/v5/mapbox/driving/${ start };${ end }?alternatives=true&geometries=geojson&overview=simplified&steps=false&access_token=${ environment.mapBoxApiKey }` ).toPromise()
+
     if ( response === undefined ) {
-      return Err( 'direction map box error' )
+      return Err(new DirectionNotFoundException())
     }
+
     const directionResult = directionFromJson( response )
 
     if ( directionResult.isErr() ) {
