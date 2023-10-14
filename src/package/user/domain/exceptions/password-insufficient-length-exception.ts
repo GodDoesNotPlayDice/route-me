f976bd81b3ada698843fdeb7c6c59de204df719c
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-14 00:41:58 -0300

    fix(user-domain): modify password exceptions

---
+++ src/package/user/domain/exceptions/password-insufficient-length-exception.ts
@@ -0,0 +1,6 @@
+export class PasswordInsufficientLengthException extends Error {
+  constructor( message?: string ) {
+    super( message )
+    this.name = 'PasswordInsufficientLengthException'
+  }
+}
---
+++ src/package/user/domain/exceptions/password-insufficient-uppercase-exception.ts
@@ -0,0 +1,6 @@
+export class PasswordInsufficientUppercaseException extends Error {
+	constructor( message?: string ) {
+		super( message )
+		this.name = 'PasswordInsufficientUppercaseException'
+	}
+}
