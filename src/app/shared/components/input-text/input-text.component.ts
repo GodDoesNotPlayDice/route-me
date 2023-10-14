343f7608a492b33052e3f34829155bab31319be0
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-14 00:48:34 -0300

    fix(input-text): modify input text validation for new domain exceptions

--- src/app/shared/components/input-text/input-text.component.ts
+++ src/app/shared/components/input-text/input-text.component.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common'
 import {
-  Component,
-  Input
+	Component,
+	Input
 } from '@angular/core'
 import {
-  FormControl,
-  ReactiveFormsModule,
-  Validators
+	FormControl,
+	ReactiveFormsModule,
+	Validators
 } from '@angular/forms'
 import { IonicModule } from '@ionic/angular'
+import { PhoneExceedsMaximumLengthException } from 'src/package/passenger/domain/exceptions/phone-exceeds-maximum-length-exception'
+import { PhoneInsufficientLengthException } from 'src/package/passenger/domain/exceptions/phone-insufficient-length-exception'
 import { newPassengerPhone } from 'src/package/passenger/domain/models/passenger-phone'
+import { PasswordInsufficientCharacterException } from 'src/package/user/domain/exceptions/password-insufficient-character-exception'
+import { PasswordInsufficientLengthException } from 'src/package/user/domain/exceptions/password-insufficient-length-exception'
+import { PasswordInsufficientLowercaseException } from 'src/package/user/domain/exceptions/password-insufficient-lowercase-exception'
+import { PasswordInsufficientNumberException } from 'src/package/user/domain/exceptions/password-insufficient-number-exception'
+import { PasswordInsufficientUppercaseException } from 'src/package/user/domain/exceptions/password-insufficient-uppercase-exception'
 import { newUserEmail } from 'src/package/user/domain/models/user-email'
 import { newUserPassword } from 'src/package/user/domain/models/user-password'
 import { z } from 'zod'
@@ -17,94 +24,114 @@ import { z } from 'zod'
 type InputTextType = 'email' | 'password' | 'text' | 'phone' | 'number'
 
 @Component( {
-  standalone : true,
-  selector   : 'app-input-text',
-  templateUrl: './input-text.component.html',
-  styleUrls  : [ './input-text.component.scss' ],
-  imports    : [
-    IonicModule,
-    CommonModule,
-    ReactiveFormsModule
-  ]
+	standalone : true,
+	selector   : 'app-input-text',
+	templateUrl: './input-text.component.html',
+	styleUrls  : [ './input-text.component.scss' ],
+	imports    : [
+		IonicModule,
+		CommonModule,
+		ReactiveFormsModule
+	]
 } )
 export class InputTextComponent {
-  readonly textControl = new FormControl( '', control => {
-    control.addValidators( Validators.required )
-    switch ( this.type ) {
-      case 'email':
-        try {
-          newUserEmail( {
-            value: control.value
-          } )
-        }
-        catch ( e ) {
-          return { email: true }
-        }
-        break
-      case 'password':
-        try {
-          newUserPassword( {
-            value: control.value
-          } )
-        }
-        catch ( e: any ) {
-          return {
-            minlength: {
-              requiredLength: e?.issues?.[0]?.['minimum']
-            }
-          }
-        }
-        break
-      case 'text':
-        control.addValidators( Validators.minLength( 3 ) )
-        break
-      case 'phone':
-        try {
-          newPassengerPhone( {
-            value: control.value
-          } )
-        }
-        catch ( e: any ) {
-          const code = e?.issues?.[0]?.['code']
-          if ( code === 'invalid_string' ) {
-            return { number: true }
-          }
-          else if ( code === 'too_small' ) {
-            return {
-              minlength: {
-                requiredLength: e?.issues?.[0]?.['minimum']
-              }
-            }
-          }
-          // else if ( code === 'too_big' ) {
-          else {
-            return {
-              maxlength: {
-                requiredLength: e?.issues?.[0]?.['maximum']
-              }
-            }
-          }
-        }
-        break
-      case 'number':
-        try {
-          z.number()
-           .parse( Number.parseInt( control.value ) )
-        }
-        catch ( e ) {
-          return { number: true }
-        }
-        break
-    }
-    return null
-  } )
+	readonly textControl = new FormControl( '', control => {
 
-  @Input() type: InputTextType = 'text'
-  @Input() focusLabel: boolean = false
-  @Input() value: string = ''
-  @Input({required:true}) placeholder: string
+		control.addValidators( Validators.required )
+		if ( this.disableErrors ) return null
+		switch ( this.type ) {
+			case 'email':
+				const emailResult = newUserEmail( {
+					value: control.value
+				} )
+				if ( emailResult.isErr() ) {
+					return { email: true }
+				}
+				break
+			case 'password':
+				const passwordResult = newUserPassword( {
+					value: control.value
+				} )
+				if ( passwordResult.isErr() ) {
+					let map: any = {}
+					for ( const error of passwordResult.unwrapErr() ) {
+						if ( error instanceof PasswordInsufficientUppercaseException ) {
+							map['uppercase'] = true
+						}
+						else if ( error instanceof
+							PasswordInsufficientLowercaseException )
+						{
+							map['lowercase'] = true
+						}
+						else if ( error instanceof PasswordInsufficientNumberException ) {
+							map['number'] = true
+						}
+						else if ( error instanceof
+							PasswordInsufficientCharacterException )
+						{
+							map['character'] = true
+						}
+						else if ( error instanceof
+							PasswordInsufficientLengthException )
+						{
+							map['minlength'] = {
+								requiredLength: error.message
+							}
+						}
+						else {
+							console.log( 'unknown password input error' )
+						}
+					}
+					return map
+				}
+				break
+			case 'text':
+				control.addValidators( Validators.minLength( 3 ) )
+				break
+			case 'phone':
+				const phoneResult = newPassengerPhone( {
+					value: control.value
+				} )
+				if ( phoneResult.isErr() ) {
+					let map: any = {}
+					for ( let error of phoneResult.unwrapErr() ) {
+						if ( error instanceof PhoneInsufficientLengthException ) {
+							map['minlength'] = {
+								requiredLength: error.message
+							}
+						}
+						else if ( error instanceof PhoneExceedsMaximumLengthException ) {
+							map['maxlength'] = {
+								requiredLength: error.message
+							}
+						}
+						else {
+							map['int'] = true
+						}
+					}
+					return map
+				}
+				break
+			case 'number':
+				try {
+					z.number()
+					 .parse( Number.parseInt( control.value ) )
+				}
+				catch ( e ) {
+					return { int: true }
+				}
+				break
+		}
+		return null
+	} )
 
-  public input( $event: Event ): void {
-    this.textControl.updateValueAndValidity()
-  }
+	@Input() type: InputTextType = 'text'
+	@Input() focusLabel: boolean = false
+	@Input() disableErrors: boolean = false
+	@Input() value: string       = ''
+	@Input( { required: true } ) placeholder: string
+
+	public input( $event: Event ): void {
+		this.textControl.updateValueAndValidity()
+	}
 }
