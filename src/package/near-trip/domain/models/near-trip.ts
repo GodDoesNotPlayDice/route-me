df1d8cc1fd9d4b3251d5409b39946c01998f4b42
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-18 22:37:23 -0300

    fix(near-trip-repository): add near trip repository and firebase impl

--- src/package/near-trip/domain/models/near-trip.ts
+++ src/package/near-trip/domain/models/near-trip.ts
@@ -1,15 +1,14 @@
 import { LocationName } from 'src/package/location/domain/models/location-name'
-import { Position } from 'src/package/position-api/domain/models/position'
 import { ValidDate } from 'src/package/shared/domain/models/valid-date'
 import { TripID } from 'src/package/trip/domain/models/trip-id'
 import { TripPrice } from 'src/package/trip/domain/models/trip-price'
 
 export interface NearTrip {
-  //TODO: id near trip?
   id: TripID
   startLocationName: LocationName
   endLocationName: LocationName
   price: TripPrice
   startDate: ValidDate
-  startPosition: Position
+  latitude: number,
+  longitude: number
 }
---
+++ src/package/near-trip/domain/repository/near-trip-repository.ts
@@ -0,0 +1,7 @@
+import { Result } from 'oxide.ts'
+import { NearTrip } from 'src/package/near-trip/domain/models/near-trip'
+import { Position } from 'src/package/position-api/domain/models/position'
+
+export abstract class NearTripRepository {
+  abstract getNearTrips( center : Position, radius : number): Promise<Result<NearTrip[], Error[]>>
+}
