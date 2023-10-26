cce48f3583d2d16ca9b5165bd1e7ea5044b131e9
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-26 01:41:19 -0300

    fix(objects-mothers): add driver car mothers

---
+++ src/test/app/stubs/object-mothers/driver-car/driver-car-id-mother.ts
@@ -0,0 +1,20 @@
+import { Result } from 'oxide.ts'
+import {
+	DriverCarID,
+	newDriverCarID
+} from 'src/package/driver-car/domain/models/driver-car-id'
+import { ulid } from 'ulidx'
+
+export class DriverCarIDMother{
+	static random() :Result<DriverCarID, Error>{
+		return newDriverCarID({
+			value: ulid()
+		})
+	}
+
+	static invalid() :Result<DriverCarID, Error>{
+		return newDriverCarID({
+			value: ''
+		})
+	}
+}
---
+++ src/test/app/stubs/object-mothers/driver-car/driver-car-mother.ts
@@ -0,0 +1,27 @@
+import {
+	Err,
+	Ok,
+	Result
+} from 'oxide.ts'
+import { DriverCar } from 'src/package/driver-car/domain/models/driver-car'
+import { DriverCarIDMother } from 'src/test/app/stubs/object-mothers/driver-car/driver-car-id-mother'
+import { DriverCarModelMother } from 'src/test/app/stubs/object-mothers/driver-car/driver-car-model-mother'
+import { DriverCarSeatMother } from 'src/test/app/stubs/object-mothers/driver-car/driver-car-seat-mother'
+
+export class DriverCarMother{
+	static random() :Result<DriverCar, Error[]>{
+		return Ok({
+			id: DriverCarIDMother.random().unwrap(),
+			model: DriverCarModelMother.random().unwrap(),
+			seat: DriverCarSeatMother.random().unwrap(),
+		})
+	}
+
+	static invalid() :Result<DriverCar, Error[]>{
+		return Err([
+			DriverCarIDMother.invalid().unwrapErr(),
+			DriverCarModelMother.invalid().unwrapErr(),
+			DriverCarSeatMother.invalid().unwrapErr(),
+		])
+	}
+}
