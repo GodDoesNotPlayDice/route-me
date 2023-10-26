3b21c0e0a60e5e2905daa9be7906ec550a46b3a8
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-25 21:54:31 -0300

    fix(objects-mothers): modify trip mother

--- src/test/app/stubs/object-mothers/trip/trip-mother.ts
+++ src/test/app/stubs/object-mothers/trip/trip-mother.ts
@@ -1,14 +1,23 @@
-import { Err,
-	Ok, Result } from "oxide.ts";
+import {
+	Err,
+	Ok,
+	Result,
+	Some
+} from 'oxide.ts'
 import { Trip } from 'src/package/trip/domain/models/trip'
+import { CategoryMother } from 'src/test/app/stubs/object-mothers/category/category-mother'
 import { TripDescriptionMother } from 'src/test/app/stubs/object-mothers/trip/trip-description-mother'
 import { TripIDMother } from 'src/test/app/stubs/object-mothers/trip/trip-id-mother'
+import { TripStateMother } from 'src/test/app/stubs/object-mothers/trip/trip-state-mother'
 
 export class TripMother{
 	static random() : Result<Trip, Error[]>{
 		return Ok({
 			id: TripIDMother.random().unwrap(),
-			description: TripDescriptionMother.random().unwrap()
+			description: TripDescriptionMother.random().unwrap(),
+			state: TripStateMother.random().unwrap(),
+			category: Some(CategoryMother.random().unwrap())
+
 		})
 	}
 
