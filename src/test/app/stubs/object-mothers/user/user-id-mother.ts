dd17147215d1056cf6c0fcd924dfd24ab8d1fffc
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-10-26 12:26:13 -0300

    fix(objects-mothers): add user mothers

---
+++ src/test/app/stubs/object-mothers/user/user-id-mother.ts
@@ -0,0 +1,20 @@
+import { Result } from 'oxide.ts'
+import {
+	newUserID,
+	UserID
+} from 'src/package/user/domain/models/user-id'
+import { ulid } from 'ulidx'
+
+export class UserIDMother {
+	static random() :Result<UserID, Error>{
+		return newUserID({
+			value: ulid()
+		})
+	}
+
+	static invalid() :Result<UserID, Error>{
+		return newUserID({
+			value: 'invalid'
+		})
+	}
+}
