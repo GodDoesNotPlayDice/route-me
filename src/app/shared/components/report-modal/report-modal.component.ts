b2b616816dae6f8f14b45dfae67b96f445852684
LR56c <ronalli56@gmail.com>
2023-11-09 01:05:27 -0300

    fix(report-modal): add report modal

---
+++ src/app/shared/components/report-modal/report-modal.component.ts
@@ -0,0 +1,17 @@
+import {
+	Component,
+	OnInit
+} from '@angular/core'
+
+@Component( {
+	selector: 'app-report-modal',
+	templateUrl: './report-modal.component.html',
+	styleUrls: [ './report-modal.component.scss' ]
+} )
+export class ReportModalComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {}
+
+}
